7113bb14fe76c0e2c35cb85253e59caae1f18fdc
DESKTOP-6U6GE0V\kosmo <dmspdlsrb@naver.com>
2022-01-04 12:58:53 +0900

    commit

--- src/main/java/edu/kosmo/ex/contoroller/SecurityController.java
+++ src/main/java/edu/kosmo/ex/contoroller/SecurityController.java
@@ -12,8 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
 
 
+@Log4j
 @RequestMapping("/security/*")
 @Controller
 public class SecurityController {
@@ -30,6 +33,25 @@ public class SecurityController {
 		
 		return "/security/member";
 	}
-
+	/*@GetMapping("/admin")
+	public String admin() {
+		log.info("login admin");
+		
+		return "/security/admin";
+	}*/
+	
+	@GetMapping("/admin")
+	public void admin() { //void로 적어줘도 경로명을 읽어 준다 security/admin 해줌
+		System.out.println("login admin");
+		
+	}
+	
+	@GetMapping("/accessError")
+	public void accessEorror(Model model) {
+		System.out.println("accessError");
+		
+		
+		model.addAttribute("msg","ACCESS Denied(403 에러)"); //리턴값이 void이므로 
+	}
 	
 }
